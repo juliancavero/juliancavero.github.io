685d835d3a7a445145a14a2003e93b011a28b63f
juliancavero <juliancavero95@gmail.com>
2024-05-29 20:28:36 +0200

    asdf

--- src/utils/router.tsx
+++ src/utils/router.tsx
@@ -6,7 +6,10 @@ const router = createHashRouter([
   {
     path: "/",
     element: <MainSkeleton />,
-    children: [{ path: "/", element: <MortgagePage /> }],
+    children: [
+      { path: "/", element: <MortgagePage /> },
+      { path: "*", element: <MortgagePage /> },
+    ],
   },
 ]);
 
